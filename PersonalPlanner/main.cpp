6a01279f9e919274259e300e3f213f6c93cb9031
sabrine-gamdou <sgamdou@stud.hs-bremen.de>
2020-05-05 04:55:53 -0700

    "test"

--- PersonalPlanner/main.cpp
+++ PersonalPlanner/main.cpp
@@ -5,7 +5,12 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+<<<<<<< HEAD
     w.show();
     std::cout << "START" << std::endl;
+=======
+//    w.show();
+
+>>>>>>> d45371f68de6e4f5bb76f11ed23ae9eeb20847d0
     return a.exec();
 }
